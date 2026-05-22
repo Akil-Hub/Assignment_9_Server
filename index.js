ba2a81246ae78884b7439afe61eee9731b2cabde
Akil-Hub <akilanjummahi@gmail.com>
2026-05-23 01:42:52 +0600

    fix some bug

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const { payload } = await jwtVerify(token, JWKS)
-    console.log(payload)
+    req.user = payload
     next()
   } catch (error) {
     return res.status(403).json({
@@ -50,183 +50,184 @@ const verifyToken = async (req, res, next) => {
     })
   }
 }
-  
-  async function run() {
-    try {
 
-      // await client.connect();
+async function run() {
+  try {
 
-
-      const db = client.db('sports_management')
-      const facilitiesCollection = db.collection('sportsFacilities')
-      const bookingCollection = db.collection('bookingList')
+    // await client.connect();
 
 
-      //  ALL GET API STARTED
+    const db = client.db('sports_management')
+    const facilitiesCollection = db.collection('sportsFacilities')
+    const bookingCollection = db.collection('bookingList')
 
 
-      app.get('/allFacilities', async (req, res) => {
-  const { search, sport } = req.query
+    //  ALL GET API STARTED
 
-  const query = {}
 
-  if (search) {
-    query.facility_name = { $regex: search, $options: 'i' }
+    app.get('/allFacilities', async (req, res) => {
+      const { search, sport } = req.query
+
+      const query = {}
+
+      if (search) {
+        query.facility_name = { $regex: search, $options: 'i' }
+      }
+
+      if (sport) {
+        query.sports_type = { $in: [sport] }
+      }
+
+      const result = await facilitiesCollection.find(query).toArray()
+      res.json(result)
+    })
+
+
+
+    // get the signle facility by id
+    app.get('/allFacilities/:id', verifyToken, async (req, res) => {
+
+      const { id } = req.params
+
+      const result = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
+      res.json(result)
+    })
+
+    // get the owner single facility 
+
+    app.get('/manageFacilities', async (req, res) => {
+
+      const result = await facilitiesCollection.find().toArray()
+      res.json(result)
+    })
+
+    // get the owner single facility 
+    app.get('/manageFacilities/:id', async (req, res) => {
+
+      const { id } = req.params
+
+      const result = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
+      res.json(result)
+    })
+
+    // Get the facility booking lists
+
+    app.get("/myBookings", verifyToken, async (req, res) => {
+
+      const userId = req.headers.userId
+
+      const result = await bookingCollection.find({ userId }).toArray()
+      res.json(result)
+    })
+
+    //  ALL GET API END
+    ///------------------------------------------------------------------------
+
+    //  ALL POST API STARTED
+    // api for add new facility
+    app.post('/allFacilities', verifyToken, async (req, res) => {
+
+      try {
+        const postData = req.body
+        const result = await facilitiesCollection.insertOne(postData)
+        res.status(201).json({
+          success: true,
+          message: 'Facility added successfully'
+        })
+      } catch (error) {
+        console.error("Insert facility error:", error);
+
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    })
+    // post req for add my bookinglist
+    app.post('/myBookings', verifyToken, async (req, res) => {
+ 
+
+      try {
+        const bookingData = req.body
+
+
+        const result = await bookingCollection.insertOne(bookingData)
+        res.status(201).json({
+          success: true,
+          message: 'Facility booked successfully'
+        })
+
+      } catch (error) {
+        console.log('Error to save my bookings facilities')
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    })
+
+    //  ALL POST API END
+    ///------------------------------------------------------------------------
+
+
+    //  ALL PATCH API STARTED
+
+    app.patch('/manageFacilities/:id', async (req, res) => {
+
+      const { id } = req.params
+
+      const updatedData = req.body
+
+
+      const result = await facilitiesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      )
+      res.json(result)
+    })
+
+
+    //  ALL PATCH API END
+    ///------------------------------------------------------------------------
+
+
+
+    //  ALL DELETE API STARTED
+
+    app.delete(`/myBookings/:id`, verifyToken, async (req, res) => {
+      const { id } = req.params
+      const result = await bookingCollection.deleteOne({ _id: new ObjectId(id) })
+      res.json(result)
+
+    })
+    app.delete(`/allFacilities/:id`, verifyToken, async (req, res) => {
+      const { id } = req.params
+      const result = await facilitiesCollection.deleteOne({ _id: new ObjectId(id) })
+      res.json(result)
+
+    })
+
+    //  ALL DELETE API ENDo
+    ///----------------------------------------------------------------------
+
+
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
+}
+run().catch(console.dir);
 
-  if (sport) {
-    query.sports_type = { $in: [sport] }
-  }
 
-  const result = await facilitiesCollection.find(query).toArray()
-  res.json(result)
+app.get('/', (req, res) => {
+  res.send('hello world Server is running fine')
 })
-      // app.get('/allFacilities', async (req, res) => {
-
-      //   const result = await facilitiesCollection.find().toArray()
-      //   res.json(result)
-      // })
-
-
-      // get the signle facility by id
-      app.get('/allFacilities/:id', verifyToken, async (req, res) => {
-
-        const { id } = req.params
-
-        const result = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
-        res.json(result)
-      })
-
-      // get the owner single facility 
-
-      app.get('/manageFacilities', async (req, res) => {
-
-        const result = await facilitiesCollection.find().toArray()
-        res.json(result)
-      })
-
-      // get the owner single facility 
-      app.get('/manageFacilities/:id', async (req, res) => {
-
-        const { id } = req.params
-
-        const result = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
-        res.json(result)
-      })
-
-      // Get the facility booking lists
-
-      app.get("/myBookings", verifyToken, async (req, res) => {
-
-        const result = await bookingCollection.find().toArray()
-        res.json(result)
-      })
-
-      //  ALL GET API END
-      ///------------------------------------------------------------------------
-
-      //  ALL POST API STARTED
-// api for add new facility
-      app.post('/allFacilities',verifyToken, async (req, res) => {
-
-        try {
-          const postData = req.body
-          const result = await facilitiesCollection.insertOne(postData)
-          res.status(201).json({
-            success: true,
-            message: 'Facility added successfully'
-          })
-        } catch (error) {
-          console.error("Insert facility error:", error);
-
-          res.status(500).json({
-            success: false,
-            message: "Internal server error",
-          });
-        }
-      })
-      // post req for add my bookinglist
-      app.post('/myBookings', async (req, res) => {
-        try {
-          const bookingData = req.body
-
-          const result = await bookingCollection.insertOne(bookingData)
-          res.status(201).json({
-            success: true,
-            message: 'Facility booked successfully'
-          })
-
-        } catch (error) {
-          console.log('Error to save my bookings facilities')
-          res.status(500).json({
-            success: false,
-            message: "Internal server error",
-          });
-        }
-      })
-
-      //  ALL POST API END
-      ///------------------------------------------------------------------------
-
-
-      //  ALL PATCH API STARTED
-
-      app.patch('/manageFacilities/:id', async (req, res) => {
-
-        const { id } = req.params
-
-        const updatedData = req.body
-
-
-        const result = await facilitiesCollection.updateOne(
-          { _id: new ObjectId(id) },
-          { $set: updatedData }
-        )
-        res.json(result)
-      })
-
-
-      //  ALL PATCH API END
-      ///------------------------------------------------------------------------
 
 
 
-      //  ALL DELETE API STARTED
-
-      app.delete(`/myBookings/:id`,verifyToken, async (req, res) => {
-        const { id } = req.params
-        const result = await bookingCollection.deleteOne({ _id: new ObjectId(id) })
-        res.json(result)
-
-      })
-      app.delete(`/allFacilities/:id`,verifyToken, async (req, res) => {
-        const { id } = req.params
-        const result = await facilitiesCollection.deleteOne({ _id: new ObjectId(id) })
-        res.json(result)
-
-      })
-
-      //  ALL DELETE API ENDo
-      ///----------------------------------------------------------------------
-
-
-
-      // await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
-  }
-  run().catch(console.dir);
-
-
-  app.get('/', (req, res) => {
-    res.send('hello world Server is running fine')
-  })
-
-
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
